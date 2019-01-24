4f5ae6e03bb72f101ace4b6884285403f998ef36
Aleksei Chyrva <alexey.chyrva@gmail.com>
2019-01-24 21:06:45 +0200

    - add context for store options - refactoring for use context instead parents props

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -1,28 +1,29 @@
 // Core
 import React, { Component } from 'react';
 
+//Components
+import { Consumer } from '../../components/HOC/withProfile';
+
 //Instruments
 import Styles from './styles.m.css';
 
 export default class StatusBar extends Component {
     render() {
-        const {
-            avatar,
-            currentUserFirstName,
-            currentUserLastName,
-        } = this.props;
-
         return (
-            <section className = { Styles.statusBar }>
-                <button>
-                    <img
-                        alt = 'avatar'
-                        src = { avatar }
-                    />
-                    <span>{ currentUserFirstName}</span>
-                    <span>{ currentUserLastName }</span>
-                </button>
-            </section>
+            <Consumer>
+                {(context) => (
+                    <section className = { Styles.statusBar }>
+                        <button>
+                            <img
+                                alt = 'avatar'
+                                src = { context.avatar }
+                            />
+                            <span>{ context.currentUserFirstName}</span>
+                            <span>{ context.currentUserLastName }</span>
+                        </button>
+                    </section>
+                )}
+            </Consumer>
         );
     }
 }
